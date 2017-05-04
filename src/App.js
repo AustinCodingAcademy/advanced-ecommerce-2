c7eb86592585a92b61eec5a00dae8012af44482b
Collind09 <collind09@gmail.com>
2017-05-03 22:12:48 -0500

    Homework 2 Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ImageSlider from './components/ImageSlider';
-import ProductDetail from './components/ProductDetail';
-import SubHeader from './components/SubHeader';
-import TopNav from './components/TopNav';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ImageSlider from "./components/ImageSlider";
+import SubHeader from "./components/SubHeader";
+import TopNav from "./components/TopNav";
+import ProductDetail from "./components/ProductDetail";
 
 function App(props) {
   function renderProducts(products){
@@ -13,100 +13,45 @@ function App(props) {
       return  <div key={index}> <ProductDetail product={product} /> </div>
     })
 
-	return_products;
+    return _products;
+  }
 
+    return (
       <div className="App">
         	<div className="wrap">
-	
-			<div className="header">
-			
-			<Header />
-
+				<div className= "header">
+            <Header />
 			<div className="clear"> </div>
-			
 			<SubHeader />
-
 			<div className="clear"> </div>
-			
-			<TopNav  />
-		
+			<TopNav changeCategory={props.changeCategory} />
 			</div>
 			
-		
-				<ImageSlider />
-				
+			            <ImageSlider />
+
+
+
+
 				<div className="content">
 					<div className="products-box">
-						<div className="products">
-							<h5><span>FEATURED</span> PRODUCTS</h5>
-							<div className="section group">
-							
-						
-							{
-								
-								props.filteredProducts.map((product, key) => {
-									return (
-										<div>
-											<div className="grid_1_of_5 images_1_of_5">
-												<ProductDetail product={product} />
-											</div>
-											{clearfix(key, 5)}
-										</div>
-									)
-								})
-							}
+					<div className="products">
+						<h5><span>FEATURED</span> PRODUCTS</h5>
+						<div className="section group">
+            {renderProducts(props.products)}
+
 						</div>
 					</div>
-						
-						
 					<div className="products products-secondbox">
-						<h5><span>Our</span> Specials</h5>
-						<div className="section group">
-							<div className="grid_1_of_5 images_1_of_5">
-								 <img src="images/g1.jpg" alt="" />
-								 <h3>Lorem Ipsum is simply </h3>
-								 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
-								 <h4><span>$600.00</span>$512.00</h4>
-							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
-						   </div>
-							<div className="grid_1_of_5 images_1_of_5">
-								 <img src="images/g6.jpg" alt="" />
-								 <h3>Lorem Ipsum is simply </h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
-								 <h4><span>$400.00</span>$352.00</h4>
-							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
-						    </div>
-							<div className="grid_1_of_5 images_1_of_5">
-								<img src="images/g7.png" alt="" />
-								 <h3>Lorem Ipsum is simply </h3>
-								 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
-								  <h4><span>$300.00</span>$202.00</h4>
-							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
-							</div>
-							<div className="grid_1_of_5 images_1_of_5">
-								 <img src="images/g8.png" alt="" />
-								 <h3>Lorem Ipsum is simply </h3>
-								 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
-								 <h4><span>$400.00</span>$322.00</h4>
-							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
-							</div>
-							<div className="grid_1_of_5 images_1_of_5">
-								 <img src="images/g1.jpg" alt="" />
-								 <h3>Lorem Ipsum is simply</h3>
-								 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, in reprehenderit.</p>
-								 <h4><span>$700.00</span>$602.00</h4>
-							     <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
-							</div>
-						</div>
+
+
 					</div>
 				</div>
 			</div>
 			<div className="clear"> </div>
-			
-			<Footer />
-
-			</div>
+      <Footer />
 		</div>
+
+      </div>
     );
 }
 
