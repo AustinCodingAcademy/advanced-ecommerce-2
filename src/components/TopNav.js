c6c6b72fa8e8b69c93b5e0dbd9dc1a1784e99fbd
stackbaxter <stackbaxter@gmail.com>
2017-08-01 13:10:58 -0500

    think that's about it...contact page maybe...

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -2,16 +2,16 @@ import React from "react";
 
 export default function TopNav(props) {
   function onClick(e) {
-    props.changeCategory(e.target.value);
+    props.changeCategory(e.target.innerHTML.toLowerCase());
   }
   return (
     <div className="top-nav">
       <ul>
-        <li><a href="#" onClick={onClick} >car lights</a></li>
-        <li><a href="carwheels.html">Car wheels</a></li>
-        <li><a href="carbumpers.html">car bumpers</a></li>
-        <li><a href="caradsystem.html">car audiosystem</a></li>
-        <li><a href="truckbumpers.html">Truck bumpers</a></li>
+        <li><a href="#" onClick={onClick}>Headlights</a></li>
+        <li><a href="#" onClick={onClick}>Tires</a></li>
+        <li><a href="#" onClick={onClick}>Bumpers</a></li>
+        <li><a href="#" onClick={onClick}>Audio</a></li>
+        <li><a href="#" onClick={onClick}>Engine</a></li>
         <li><a href="contact.html">Feedback</a></li>
         <div className="clear" />
       </ul>
