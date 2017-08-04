a799dd2e0391155a25b3ed0e1fd4982e93ada759
Nancy Montiel <nancysarahi@Nancys-MacBook-Pro.local>
2017-08-04 01:40:17 -0500

    fixed links

--- src/App.js
+++ src/App.js
@@ -8,17 +8,18 @@ import SubHeader from "./components/SubHeader";
 import ProductDetail from "./components/ProductDetail"
 
 function App(props) {
+
 	let filteredProducts = props.products;
 	function filterProducts(category) {
 		filteredProducts = props.products.filter((product) => {
-			return props.products.category === category;
+			return product.category === category;
 		});
 		return filteredProducts.map((product) => {
 			return <ProductDetail key={product.id} product={product} />;
 		});
 	}
 
-	let productDetails = filterProducts(props.products.currentCategory);
+	let productDetails = filterProducts(props.currentCategory);
 	return (
 		<div className="App">
 			<div className="wrap">
@@ -27,7 +28,7 @@ function App(props) {
 					<div className="clear"> </div>
 					<SubHeader />
 					<div className="clear"> </div>
-					<TopNav changeCategory={props.changeCategory} />
+					 <TopNav changeCategory={props.changeCategory} /> 
 				</div>
 
 				<ImageSlider />
