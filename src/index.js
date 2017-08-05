037fff7aa1eb61dd7260fd0a2a157ddfcc6fa710
ahornbeck268 <ahornbeck268@gmail.com>
2017-08-05 11:31:03 -0500

    init commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./App.js";
 import "./index.css";
 import state from "./state";
 
+
+var currentCategory = [];
+function changeCategory(category) {
+  return currentCategory.push(category);
+}
+
 function render() {
   ReactDOM.render(
-    <App state={state} />,
+    <App state={state} changeCategory={changeCategory} currentCategory={currentCategory} />,
     document.getElementById("root")
   );
 }
 render();
-
