f708f46ec6ba662788702f8b18fe8582d0379291
CGottschallJ <Cameron.Gottschall@gmail.com>
2017-10-18 21:25:21 -0500

    second: through map, filter remains

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ProductDetail(props) {
+  const {id, name, description, price, imgUrl, category, rating, reviews} = props.product;
+
+    return(
+      <div className="grid_1_of_5 images_1_of_5">
+         <img src={imgUrl} />
+         <h3>{name}</h3>
+         <p>{description}</p>
+         <h4>{price}</h4>
+           <div className="button"><span><a href="singlepage.html">Read More</a></span></div>
+      </div>
+    );
+}
+
+ProductDetail.propTypes = {
+  product: PropTypes.object.isRequired
+}
+
+export default ProductDetail;
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -4,12 +4,12 @@ function TopNav() {
   return (
     <div className="top-nav">
       <ul>
-        <li><a href="carlights.html">car lights</a></li>
-        <li><a href="carwheels.html">Car wheels</a></li>
-        <li><a href="carbumpers.html">car bumpers</a></li>
-        <li><a href="caradsystem.html">car audiosystem</a></li>
-        <li><a href="truckbumpers.html">Truck bumpers</a></li>
-        <li><a href="contact.html">Feedback</a></li>
+        <li><a href="#">car lights</a></li>
+        <li><a href="#">Car wheels</a></li>
+        <li><a href="#">car bumpers</a></li>
+        <li><a href="#">car audiosystem</a></li>
+        <li><a href="#">Truck bumpers</a></li>
+        <li><a href="#">Feedback</a></li>
         <div className="clear"> </div>
       </ul>
     </div>
