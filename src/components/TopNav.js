eacec33295fc021f49726d3eadd127e461c1500d
Andrew Hartford <andrewhartford@utexas.edu>
2017-10-23 07:28:51 -0500

    finished hw

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -4,12 +4,38 @@ function TopNav() {
     return(
     <div className="top-nav">
     <ul>
-        <li><a href="carlights.html">car lights</a></li>
-        <li><a href="carwheels.html">Car wheels</a></li>
-        <li><a href="carbumpers.html">car bumpers</a></li>
-        <li><a href="caradsystem.html">car audiosystem</a></li>
-        <li><a href="truckbumpers.html">Truck bumpers</a></li>
+        <li><a href="#" onClick={
+            (a) => {
+                a.preventDefault();
+                props.changeCategory("headlights");
+            }}>car lights</a></li>
+
+        <li><a href="#" onClick={
+            (a) => {
+                a.preventDefault();
+                props.changeCategory("tires");
+            }}>Car Wheels</a></li>
+
+        <li><a href="#" onClick={
+            (a) => {
+                a.preventDefault();
+                props.changeCategory("bumpers");
+            }}>Car Bumpers</a></li>
+
+        <li><a href="#" onClick={
+            (a) => {
+                a.preventDefault();
+                props.changeCategory("audio");
+            }}>car audiosystem</a></li>
+
+        <li><a href="#" onClick={
+            (a) => {
+                a.preventDefault();
+                props.changeCategory("bump=ers");
+            }}>Truck Bumpers</a></li>
+            
         <li><a href="contact.html">Feedback</a></li>
+     
         <div className="clear"> </div>
     </ul>
 </div>
